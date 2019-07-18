fa6f87353212810256994c93a56283f40b6999b1
Adnan <adnan.chowdhury@outlook.com>
2019-07-17 22:15:34 -0500

    Visual tweak

--- src/components/cardComponents/image.js
+++ src/components/cardComponents/image.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Image(props){
     return(
-        <img src ={props.src} alt ="NASA thing" style={{maxWidth: "250px"}}/>
+        <img src ={props.src} alt ="NASA thing" style={{maxWidth: "400px"}}/>
         //<div>Image to be passed via</div>
     )
 }
