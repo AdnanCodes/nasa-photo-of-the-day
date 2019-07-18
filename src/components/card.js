9717d4ce4bf7af8bcf26d821c63907cb489e5627
Adnan <adnan.chowdhury@outlook.com>
2019-07-18 10:20:02 -0500

    Added a loading screen but it loads so fast, I can't test it

--- src/components/card.js
+++ src/components/card.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Details, Image, Title } from './cardComponents'
 
 export default function Card(props) {
+
+    if(!props.passData ){
+        return(
+            <div>{console.log(props)}
+                Page is loading, Please Wait...
+            </div>
+        )
+    }
     return (
         <div>
             <Title title={props.passData.title} />
