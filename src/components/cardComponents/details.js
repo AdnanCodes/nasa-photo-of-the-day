a86a8e1a238a5e0b44041d3ddf235331904c7573
Adnan <adnan.chowdhury@outlook.com>
2019-07-18 16:36:36 -0500

    Updated packages, Added semantic ui react and styled componted, applied simple styles

--- src/components/cardComponents/details.js
+++ src/components/cardComponents/details.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Message } from "semantic-ui-react";
+import styled from "styled-components";
+
 
 export default function Details(props) {
+
+    const SizeAdjust = styled.div`
+    width: 60%;
+    margin: 0 auto;
+    `;
+
     return (
-        <p>{props.detail}
-        </p>
+        <SizeAdjust>
+        <Message>
+            <Message.Header>About this picture</Message.Header>
+            {props.detail}
+        </Message>
+        </SizeAdjust>
     )
 }
 
--- src/components/cardComponents/title.js
+++ src/components/cardComponents/title.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from "styled-components";
+
+
+
 
 export default function Title(props) {
+    const StyleH1 = styled.h1`
+    color: #008000;
+    font-size: 4rem;
+    `;
     return (
-        <h2>{props.title}</h2>
+        <StyleH1>{props.title}</StyleH1>
     )
 }
